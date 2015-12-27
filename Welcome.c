437f1885cc56ebb1a4f8e59e0ac5713971ba0fbf
gleb812 <gleb.rogozinsky@gmail.com>
2015-12-27 17:59:38 +0300

    Welcome edited

--- Welcome.c
+++ Welcome.c
@@ -2,6 +2,8 @@
 
 void Welcome(void)
 {
+    printf("=====================&===========================n");
     printf("Clavia Nord Modular G2 patch converter, v. 1.0.1\n");
     printf("(c) Gleb G. Rogozinsky, Michael Chesnokov 2015\n");
+    printf("=====================&===========================n");
 }
