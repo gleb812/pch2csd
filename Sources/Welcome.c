330421aa61b0764d0a6cdc058c90d59b05c4ac51
Eugene Cherny <iam@oscii.ru>
2017-04-14 20:07:11 +0300

    [+] new concise command-line help. no elephats, sorry @gleb812

--- Sources/Welcome.c
+++ Sources/Welcome.c
@@ -1,40 +1,11 @@
 #include <stdio.h>
 
 void Help(void) {
+    printf("Clavia Nord Modular G2 patch to Csound converter, ver. 0.1\n");
     printf("\n");
-    printf("///////////////////osyyo///////////////////////////////////////////////////////\n");
-    printf("//////////////+oshmNMMMmNmhyso+//////////////////+++++////////////////////+o///\n");
-    printf("///////////+oshydNMMMMMMMMMMMNh++++++++++ossssyysssyshhys+//////////////+hNmhys\n");
-    printf("/////////+oydmmNNNNNNNNNNNMMNNdhyyyyyyyhhddy+++::/ooossyyhs+///////////omMMMNNN\n");
-    printf("///////+oyyhhhdhhhhhhdmdmmddhyshhyysys++ossys+--+syyyyyysoo++/////////+hmmmmmdy\n");
-    printf("///+++++++++ooooooosossssoo+++syyyssyyss+-..`.ydmmddyo+/+++/++++++++++shhhhhyys\n");
-    printf("+++                        oooyhyyhyyyhhdso+/:sddyo+//++/////++++++++++ooosssss\n");
-    printf("+++ Clavia Nord Modular G2 sshhhyyyyyys+-+hho/ys+///++/////:+++++++++++++++++++\n");
-    printf("+++     Patch Converter    ooossosyyy+:``.--`.//+/+/://+/o+++++++++++++++++++++\n");
-    printf("+++                        oo+oysysso/:-.``````.-:/+/-/+syso+++++++++++++++++++\n");
-    printf("++oooooooooooooooooooooooooooosssysoosys+-``` ``-:////://oosooooooooo++++++++++\n");
-    printf("ooooooooooooosssssooosssssssssshyyso+shdh.`    `-/:-:-:--/++ooooooooooooooooyso\n");
-    printf("ssssssssyyyyyyyyyyyyyyyyyssssooooso+++yhh-     .:/--````-::-/oooooooooooosyhhdd\n");
-    printf("ossosssssssssssssssssssssssss/++++/--/+hs`   `.`-...````-..`oooooooosssyssssyyy\n");
-    printf("ooooooosssssssssssssssyysssss/////-`  sNm     `   .`   ``` /oooosoosyhdhysooyhd\n");
-    printf("oooooosssssssssssssshdyysssym/:::-`/.:mmo         `       :sssssyyyyyysoosyyyyy\n");
-    printf("osssssssssssssssyyhdy+++shmNs-.```.Ny``                   +ssssyyyhhyyyyyssssoo\n");
-    printf("sssshddhysyssyyyhdds/oyhsdysh-.   omm-       -.          :.+yssssyyyyysyhyyysyh\n");
-    printf("yhhhdhhhhhyyyyyhhhh/.:ysydmNh.   .hmmy      `:-`  ``    `yo-ohyyyyyyyyyyyysssss\n");
-    printf("syyyyyyyyyyhddhddmmy.`.:/++o/`  `yhhdh.     ..` ````    ohhyyyyyyyyyyyyyyyyyyss\n");
-    printf("hysyyyyhhhyhhhhhyyhhy/`  `-:. `/yyhhhyo    `.` ``      +yyyyyyyyyyyyysyssssssss\n");
-    printf("hyyhhyyhhdhhhhyyyyyyyyyo+///+syyyyyyyhy-   ..``:yo`   :hhyyyyysyyyssyysssssssss\n");
-    printf("yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyo  .-.``syy-   syyyyyyssssssssssssssssss\n");
-    printf("ssssyyyyyyyyyyyyyyyysssssosssoooooooooos-`--.`-sss.  `ssssooooooooooooooooooooo\n");
-    printf("sssyyysssssssssssssooooooooossssssssssss+-:-.`oysy   -yyyyyyyyyyyyyyyyyysyysyyy\n");
-    printf("yyyyyyyyyyyyyhhhhyhhhhhdhhdddddddhdddddd/.:-``yddy   :ddddhhdddddddddhhhhhhdhhh\n");
-    printf("hhddddddddddddddddddddddmmmmmmmdddmmdddh/.:.../hd+   .ddddddddddddhhhyhhhhhhhhh\n");
-    printf("hhhhhhhhhhhhdddhdddddddddhhhhhhhhdhhhhhh-.o+/--hy.    -osyhhhddddhhhhyyyyyyyyys\n");
-    printf("dddhhhhhhhhhhhhdddhdhhhhhhhhhhyyyssyo//:`-hyys:` ```.-::/+osyyysyyyyyyyyyhyyhys\n");
-    printf("hhhyyhhhhhdhddhhhhyyysosoysosooo//:----.`+yysssssossooyyysyhhhhyhhyyyyyyhhyyyyy\n");
+    printf("Usage:\n");
+    printf("  pch2csd nm2_patch_file.pch2 [-d data_dir]\n");
     printf("\n");
-
-
-    printf("Clavia Nord Modular G2 patch converter, v. 1.0.1\n");
-    printf("(c) Gleb G. Rogozinsky, Michael Chesnokov 2015\n");
+    printf("Options:\n");
+    printf("  -d Path to a data-directory (maps, UDOs, etc.). Default is './Data/'\n");
 }
