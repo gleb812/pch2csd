9a0fb55fbd06fe28a879f6d8be99d36e395fb004
Eugene Cherny <iam@oscii.ru>
2017-05-26 14:12:14 +0300

    [+] equality testing test

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -19,3 +19,16 @@ class TestParsing(TestCase):
         parsed = parse_pch2(gleb_2_pch)
         self.assertSequenceEqual(parsed.modules, expected_modules)
         self.assertSequenceEqual(parsed.cables, expected_cables)
+
+    def test_gleb2_pch__unequal(self):
+        gleb_2_pch = get_test_resource('Gleb2.pch2')
+        expected_modules_correct = [Module(Location.VOICE_AREA, 4, 1),
+                                    Module(Location.VOICE_AREA, 96, 2)]
+        expected_modules = [Module(Location.VOICE_AREA, 4, 1),
+                            Module(Location.VOICE_AREA, 123, 2)]
+        expected_cables = [
+            Cable(Location.VOICE_AREA, CableType.IN_TO_IN, CableColor.WHITE, 1, 0, 141, 1)]
+        parsed = parse_pch2(gleb_2_pch)
+        self.assertTrue(parsed.modules == expected_modules_correct)
+        self.assertFalse(parsed.modules == expected_modules)
+        self.assertFalse(parsed.cables == expected_cables)
