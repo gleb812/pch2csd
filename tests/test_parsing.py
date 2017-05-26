fe767c65bd6d7fe9c52bc9433f69646b5b3c2c9a
Eugene Cherny <iam@oscii.ru>
2017-05-26 04:24:31 +0300

    [+] cable list parse test (passing)

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,16 +1,21 @@
 import os
-from unittest import TestCase
 
 from pch2csd.parsing.parsing import parse_pch2
-from pch2csd.parsing.structs import Module
+from pch2csd.parsing.structs import Module, Location, Cable, CableType, CableColor
 
 
 def get_test_resource(path: str) -> str:
     return os.path.join(os.path.dirname(__file__), 'resources', path)
 
+
 class TestParsing(TestCase):
     def test_gleb2_pch(self):
         gleb_2_pch = get_test_resource('Gleb2.pch2')
-        expected_modules = [Module(4, 1, 'voice'), Module(96, 2, 'voice')]
+        expected_modules = [Module(Location.VOICE_AREA, 4, 1),
+                            Module(Location.VOICE_AREA, 96, 2)]
+        expected_cables = [
+            Cable(Location.VOICE_AREA, CableType.IN_TO_IN, CableColor.WHITE, 1, 0, 1, 1)]
         parsed = parse_pch2(gleb_2_pch)
         self.assertSequenceEqual(parsed.modules, expected_modules)
+        self.assertSequenceEqual(parsed.cables, expected_cables)
+
