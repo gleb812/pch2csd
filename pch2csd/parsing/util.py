afe736e7b055addd0908664e89f05e7a291a985f
Eugene Cherny <iam@oscii.ru>
2017-05-28 00:33:56 +0300

    [+] BitArrayStream stream wrapper for for bitarray

--- pch2csd/parsing/util.py
+++ pch2csd/parsing/util.py
@@ -1,3 +1,9 @@
+from struct import unpack
+from typing import List
+
+from bitarray import bitarray
+
+
 class AttrEqMixin:
     def attrs_equal(self, other):
         if type(self) != type(other):
@@ -18,3 +24,27 @@ class ReprStrMixin:
 
     def __str__(self):
         return self.__repr__()
+
+
+class BitArrayStream:
+    def __init__(self, bits: bitarray):
+        self.pos = 0
+        self.bits = bits
+
+    def _read_int(self, int_len: int):
+        b = self.bits[self.pos:self.pos + int_len].tobytes()
+        if 0 < int_len <= 8:
+            num = unpack('>B', b)[0]
+            num = num >> 8 - int_len
+        elif 8 < int_len <= 16:
+            num = unpack('>H', b)[0]
+            num = num >> 16 - int_len
+        else:
+            raise NotImplementedError
+        self.pos += int_len
+        return num
+
+    def read_ints(self, bit_chunks: List[int]):
+        if sum(bit_chunks) + self.pos > len(self.bits):
+            raise ValueError("Don't have enough data to read")
+        return [self._read_int(i) for i in bit_chunks]
