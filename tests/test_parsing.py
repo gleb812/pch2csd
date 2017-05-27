e574a36b2f86c5bfeab42c3aa10277337fc80a05
Eugene Cherny <iam@oscii.ru>
2017-05-28 00:34:45 +0300

    [+] module parameters parsing test (passing}

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -2,7 +2,7 @@ import os
 from unittest import TestCase
 
 from pch2csd.parsing.g2 import parse_pch2
-from pch2csd.parsing.structs import Module, Location, Cable, CableType, CableColor
+from pch2csd.parsing.structs import Module, Location, Cable, CableType, CableColor, ModuleParameters
 
 
 def get_test_resource(path: str) -> str:
@@ -14,11 +14,14 @@ class TestParsing(TestCase):
         gleb_2_pch = get_test_resource('Gleb2.pch2')
         expected_modules = [Module(Location.VOICE_AREA, 4, 1),
                             Module(Location.VOICE_AREA, 96, 2)]
-        expected_cables = [
-            Cable(Location.VOICE_AREA, CableType.IN_TO_IN, CableColor.WHITE, 1, 0, 1, 1)]
+        expected_cables = [Cable(Location.VOICE_AREA, CableType.IN_TO_IN,
+                                 CableColor.WHITE, 1, 0, 1, 1)]
+        expected_mod_params = [ModuleParameters(Location.VOICE_AREA, 1, 3, [0, 1, 0]),
+                               ModuleParameters(Location.VOICE_AREA, 2, 5, [127, 127, 0, 2, 0])]
         parsed = parse_pch2(gleb_2_pch)
         self.assertSequenceEqual(parsed.modules, expected_modules)
         self.assertSequenceEqual(parsed.cables, expected_cables)
+        self.assertSequenceEqual(parsed.mod_params, expected_mod_params)
 
     def test_gleb2_pch__unequal(self):
         gleb_2_pch = get_test_resource('Gleb2.pch2')
