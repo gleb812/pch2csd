0801fc22f103e9d3a00e30f3a982cc6e464ec300
Eugene Cherny <iam@oscii.ru>
2017-06-25 23:01:26 +0300

    [fix] missing __init__ in 'data' module

---
+++ pch2csd/data/__init__.py
@@ -0,0 +1,37 @@
+import json
+import os
+from typing import Dict, Any
+
+
+def _read_json(filename: str) -> Dict[str, Any]:
+    path = os.path.join(os.path.dirname(__file__), filename)
+    with open(path, 'r') as f:
+        return json.load(f)
+
+
+class ProjectData:
+    def __init__(self):
+        self._value_maps = None
+        self._mod_type_connection = None
+        self._mod_type_name = None
+
+    @property
+    def value_map(self):
+        if self._value_maps is None:
+            self._value_maps = _read_json('value_maps.json')
+        return self._value_maps
+
+    @property
+    def mod_type_connection(self):
+        if self._mod_type_connection is None:
+            self._mod_type_connection = {int(k): v for k, v
+                                         in _read_json('mod_type_connections.json').items()}
+        return self._mod_type_connection
+
+    @property
+    def mod_type_name(self):
+        if self._mod_type_name is None:
+            self._mod_type_name = {int(k): v for k, v
+                                   in _read_json('mod_type_name.json').items()
+                                   if v != 'Unknown'}
+        return self._mod_type_name
