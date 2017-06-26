e18b247c52eae1a6a83ab3ab5334af54a61e4d00
Eugene Cherny <iam@oscii.ru>
2017-06-26 19:24:10 +0300

    [r] cleanup unimplemented unit test

--- tests/test_csdgen.py
+++ tests/test_csdgen.py
@@ -1,15 +1,1 @@
-from unittest import TestCase
 
-from pch2csd.data import ProjectData
-from pch2csd.parsing import parse_pch2
-from tests.util import get_test_resource
-
-
-class TestCableTracing(TestCase):
-    def setUp(self):
-        self.data = ProjectData()
-
-    def test_tracing_3osc(self):
-        pch2 = get_test_resource('test_3osc.pch2')
-        patch = parse_pch2(self.data, pch2)
-        print('lal')
