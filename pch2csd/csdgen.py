90563d0c0a2820bc5d71fe1041e03ef20d6ed026
Eugene Cherny <iam@oscii.ru>
2017-06-28 18:07:35 +0300

    [+] more informative Udo.__repr__()

--- pch2csd/csdgen.py
+++ pch2csd/csdgen.py
@@ -70,7 +70,7 @@ class Udo(LogMixin):
         self.inlets, self.outlets = self._init_zak_connections()
 
     def __repr__(self):
-        return self.get_name()
+        return f'{self.get_name()}(type={self.mod.type}, id={self.mod.id})'
 
     @property
     def header(self):
