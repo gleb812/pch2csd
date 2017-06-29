e66515dfc6a9e1199b9de154e21448021e99280e
Eugene Cherny <iam@oscii.ru>
2017-06-29 18:03:35 +0300

    [+] app tests

---
+++ tests/test_app.py
@@ -0,0 +1,18 @@
+from glob import glob
+from unittest import TestCase
+
+from pch2csd.app import print_pch2, convert_pch2
+from tests.util import get_test_resource
+
+
+class TestApp(TestCase):
+    def test_print_pch2__not_raises(self):
+        for f in glob(get_test_resource('*.pch2')):
+            print(f)
+            print_pch2(f)
+
+    def test_convert_pch2__not_raises(self):
+        try:
+            convert_pch2(get_test_resource('test_in2in.pch2'))
+        except SystemExit:
+            pass  # allow program to exit while we haven't created all modules
