87125858d2644c8c24cf89f66a5d5241ed4e9f0b
Eugene Cherny <iam@oscii.ru>
2017-06-29 15:25:36 +0300

    [fix] removed string interpolation from the resource package

--- pch2csd/resources/__init__.py
+++ pch2csd/resources/__init__.py
@@ -8,11 +8,11 @@ def get_template_dir() -> str:
 
 
 def get_template_path(name: str) -> str:
-    return os.path.join(os.path.dirname(__file__), 'templates', f'{name}.txt')
+    return os.path.join(os.path.dirname(__file__), 'templates', '{}.txt'.format(name))
 
 
 def get_template_module_path(name: int) -> str:
-    return os.path.join(os.path.dirname(__file__), 'templates', 'modules', f'{name}.txt')
+    return os.path.join(os.path.dirname(__file__), 'templates', 'modules', '{}.txt'.format(name))
 
 
 def _read_json(filename: str) -> Dict[str, Any]:
