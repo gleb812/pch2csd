7cfc500b488e20eb03fa858bd5520ce6572af772
Eugene Cherny <iam@oscii.ru>
2017-06-30 02:24:27 +0300

    [r] packaging script now simpler

--- package.sh
+++ package.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-rm -rf build dist pch2csd_package
-mkdir pch2csd_package
-cd pch2csd_package
-cp -r ../* .
-python setup.py sdist bdist_wheel
-# twine upload dist/*
+rm -rf build dist && python setup.py sdist bdist_wheel && twine upload dist/*
