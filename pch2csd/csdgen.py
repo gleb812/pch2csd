21648a2f84c91d566a304925aeae73d78ea74d6b
Eugene Cherny <iam@oscii.ru>
2018-03-04 23:37:02 +0200

    [fix] couldn’t replace opcode name when `;@ TODO` is after `;@ args`

--- pch2csd/csdgen.py
+++ pch2csd/csdgen.py
@@ -168,12 +168,17 @@ class Udo:
         if len(self.tpl.args) < 2:
             return '\n'.join(self.tpl.lines[self.tpl.args_lines[0]:])
         offset = self.tpl.args_lines[self.udo_variant]
+
         udo_src = []
-        for l in self.tpl.lines[offset:]:
-            udo_src.append(l)
-            if l.strip().startswith('endop'):
-                break
-        udo_src[1] = udo_src[1].replace(self.mod.type_name, self.get_name())
+        for line in self.tpl.lines[offset:]:
+            l = line.strip()
+            if l.startswith('opcode'):
+                udo_src.append(line.replace(self.mod.type_name, self.get_name()))
+            else:
+                udo_src.append(line)
+                if l.startswith('endop'):
+                    break
+
         return '\n'.join(udo_src).strip()
 
     def _choose_udo_variant(self) -> int:
